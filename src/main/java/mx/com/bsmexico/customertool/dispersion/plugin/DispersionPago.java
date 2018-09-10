a785640454e86a8947cb518ad75ba5a40f9ac9a9
Jchr <ing.jchr2427@gmail.com>
2018-09-09 22:43:53 -0500

    Reporte Dispersión pagos

---
+++ src/main/java/mx/com/bsmexico/customertool/dispersion/plugin/DispersionPago.java
@@ -0,0 +1,280 @@
+package mx.com.bsmexico.customertool.dispersion.plugin;
+
+/**
+ * @author jchr
+ *
+ */
+public class DispersionPago {
+
+	private String cliente;
+	private String usuario;
+	private String cuentaRetiro;
+	private String beneficiario;
+	private String rfc;
+	private String cuentaDeposito;
+	private String banco;
+	private Double importe;
+	private String moneda;
+	private Double iva;
+	private Double comision;
+	private Double ivaComision;
+	private String referencia;
+	private String motivoPago;
+	private String fechaHoraOperacion;
+	private String folioOperacion;
+	private String claveRastreo;
+	private String estadoOperacion;
+
+	/**
+	 * @return the cliente
+	 */
+	public String getCliente() {
+		return cliente;
+	}
+
+	/**
+	 * @param cliente the cliente to set
+	 */
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+
+	/**
+	 * @return the usuario
+	 */
+	public String getUsuario() {
+		return usuario;
+	}
+
+	/**
+	 * @param usuario the usuario to set
+	 */
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	/**
+	 * @return the cuentaRetiro
+	 */
+	public String getCuentaRetiro() {
+		return cuentaRetiro;
+	}
+
+	/**
+	 * @param cuentaRetiro the cuentaRetiro to set
+	 */
+	public void setCuentaRetiro(String cuentaRetiro) {
+		this.cuentaRetiro = cuentaRetiro;
+	}
+
+	/**
+	 * @return the beneficiario
+	 */
+	public String getBeneficiario() {
+		return beneficiario;
+	}
+
+	/**
+	 * @param beneficiario the beneficiario to set
+	 */
+	public void setBeneficiario(String beneficiario) {
+		this.beneficiario = beneficiario;
+	}
+
+	/**
+	 * @return the rfc
+	 */
+	public String getRfc() {
+		return rfc;
+	}
+
+	/**
+	 * @param rfc the rfc to set
+	 */
+	public void setRfc(String rfc) {
+		this.rfc = rfc;
+	}
+
+	/**
+	 * @return the cuentaDeposito
+	 */
+	public String getCuentaDeposito() {
+		return cuentaDeposito;
+	}
+
+	/**
+	 * @param cuentaDeposito the cuentaDeposito to set
+	 */
+	public void setCuentaDeposito(String cuentaDeposito) {
+		this.cuentaDeposito = cuentaDeposito;
+	}
+
+	/**
+	 * @return the banco
+	 */
+	public String getBanco() {
+		return banco;
+	}
+
+	/**
+	 * @param banco the banco to set
+	 */
+	public void setBanco(String banco) {
+		this.banco = banco;
+	}
+
+	/**
+	 * @return the importe
+	 */
+	public Double getImporte() {
+		return importe;
+	}
+
+	/**
+	 * @param importe the importe to set
+	 */
+	public void setImporte(Double importe) {
+		this.importe = importe;
+	}
+
+	/**
+	 * @return the moneda
+	 */
+	public String getMoneda() {
+		return moneda;
+	}
+
+	/**
+	 * @param moneda the moneda to set
+	 */
+	public void setMoneda(String moneda) {
+		this.moneda = moneda;
+	}
+
+	/**
+	 * @return the iva
+	 */
+	public Double getIva() {
+		return iva;
+	}
+
+	/**
+	 * @param iva the iva to set
+	 */
+	public void setIva(Double iva) {
+		this.iva = iva;
+	}
+
+	/**
+	 * @return the comision
+	 */
+	public Double getComision() {
+		return comision;
+	}
+
+	/**
+	 * @param comision the comision to set
+	 */
+	public void setComision(Double comision) {
+		this.comision = comision;
+	}
+
+	/**
+	 * @return the ivaComision
+	 */
+	public Double getIvaComision() {
+		return ivaComision;
+	}
+
+	/**
+	 * @param ivaComision the ivaComision to set
+	 */
+	public void setIvaComision(Double ivaComision) {
+		this.ivaComision = ivaComision;
+	}
+
+	/**
+	 * @return the referencia
+	 */
+	public String getReferencia() {
+		return referencia;
+	}
+
+	/**
+	 * @param referencia the referencia to set
+	 */
+	public void setReferencia(String referencia) {
+		this.referencia = referencia;
+	}
+
+	/**
+	 * @return the motivoPago
+	 */
+	public String getMotivoPago() {
+		return motivoPago;
+	}
+
+	/**
+	 * @param motivoPago the motivoPago to set
+	 */
+	public void setMotivoPago(String motivoPago) {
+		this.motivoPago = motivoPago;
+	}
+
+	/**
+	 * @return the fechaHoraOperacion
+	 */
+	public String getFechaHoraOperacion() {
+		return fechaHoraOperacion;
+	}
+
+	/**
+	 * @param fechaHoraOperacion the fechaHoraOperacion to set
+	 */
+	public void setFechaHoraOperacion(String fechaHoraOperacion) {
+		this.fechaHoraOperacion = fechaHoraOperacion;
+	}
+
+	/**
+	 * @return the folioOperacion
+	 */
+	public String getFolioOperacion() {
+		return folioOperacion;
+	}
+
+	/**
+	 * @param folioOperacion the folioOperacion to set
+	 */
+	public void setFolioOperacion(String folioOperacion) {
+		this.folioOperacion = folioOperacion;
+	}
+
+	/**
+	 * @return the claveOperacion
+	 */
+	public String getClaveRastreo() {
+		return claveRastreo;
+	}
+
+	/**
+	 * @param claveOperacion the claveOperacion to set
+	 */
+	public void setClaveRastreo(String claveOperacion) {
+		this.claveRastreo = claveOperacion;
+	}
+
+	/**
+	 * @return the estadoOperacion
+	 */
+	public String getEstadoOperacion() {
+		return estadoOperacion;
+	}
+
+	/**
+	 * @param estadoOperacion the estadoOperacion to set
+	 */
+	public void setEstadoOperacion(String estadoOperacion) {
+		this.estadoOperacion = estadoOperacion;
+	}
+
+}
